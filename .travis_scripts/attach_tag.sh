28f613810a75fad6682431b7dd2934001edd91c9
kiyomura <kiyomura@dwango.co.jp>
2019-03-27 21:32:51 +0900

    test ?

--- .travis_scripts/attach_tag.sh
+++ .travis_scripts/attach_tag.sh
@@ -14,6 +14,10 @@ git config --global user.email "kiyomura@dwango.co.jp"
 git config --global hub.protocol "https"
 git config --global credential.helper "store --file=$CREDENTIAL_FILE"
 
+DIR=travis-broken-example
+
 TAG_NAME="alpha_"`date "+%Y%m%d_%H%M%S"`
 git tag -a $TAG_NAME -m 'alpha_release'
+if [ $? ne 0 ]; then exit 233; fi
 git push origin $TAG_NAME
+if [ $? ne 0 ]; then exit 199; fi
