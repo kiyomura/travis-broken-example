0b5afe47867d416c626d90d0ae2813dd81c15d94
kiyomura <kiyomura@dwango.co.jp>
2019-03-29 19:32:57 +0900

    z

--- .travis_scripts/build.sh
+++ .travis_scripts/build.sh
@@ -1,24 +1,27 @@
 ﻿#!/bin/sh
 
-CREDENTIAL_FILE="$HOME/.config/git-credential"
-mkdir -p $HOME/.config
+#CREDENTIAL_FILE="$HOME/.config/git-credential"
+#mkdir -p $HOME/.config
 
-echo "https://${GH_TOKEN}:@github.com" > $CREDENTIAL_FILE
+#echo "https://${GH_TOKEN}:@github.com" > $CREDENTIAL_FILE
 #echo "github.com:
 #- oauth_token: $GH_TOKEN
 #  user: $GH_USER"
 #> $HOME/.config/hub
 
-git config --global user.name "kiyomura"
-git config --global user.email "kiyomura@dwango.co.jp"
-git config --global hub.protocol "https"
-git config --global credential.helper "store --file=$CREDENTIAL_FILE"
+#git config --global user.name "kiyomura"
+#git config --global user.email "kiyomura@dwango.co.jp"
+#git config --global hub.protocol "https"
+#git config --global credential.helper "store --file=$CREDENTIAL_FILE"
 
-echo $TRAVIS_EVENT_TYPE
-echo $TRAVIS_BRANCH
+echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
 
+echo '------'
 pwd
+which ruby
+echo '------'
 
-git log --oneline -n 20
+git log --oneline -n 200 --decorate --pretty=format:'[%cd]%d' --date=format:'%Y/%m/%d %H:%M:%S'
 
 exit 0
