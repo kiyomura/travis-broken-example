627ad0b1600d3f310814ef39139cfcd30e242bbb
kiyomura <kiyomura@dwango.co.jp>
2019-03-30 22:34:17 +0900

    hoge

---
+++ .travis_scripts/build.rb
@@ -0,0 +1,42 @@
+﻿require 'date'
+require 'time'
+
+BUILD_AT = 18 * 60 * 60
+# 200履歴とればさすがに1件は alpha タグついたものあるでしょう
+MAX_OF_SEARCHING_HISTORY = 200
+
+class Symbol
+  def call(*args)
+    case
+      when args.size > 0 then -> arg {self.to_proc[arg, *args]}
+      else self.to_proc
+    end
+  end
+end
+
+def cron?
+  ENV['TRAVIS_EVENT_TYPE'] == 'cron'
+end
+
+def with_buildable_tag?
+  /^(.{19}).+\(.*(alpha_[^,]+),.*\)$/ =~ <<-`EOS`
+      git log --oneline -n #{MAX_OF_SEARCHING_HISTORY} --decorate \
+          --pretty=format:'%cd%d' --date=format:'%Y/%m/%d %H:%M:%S'
+    EOS
+      .split("\n")
+      .select(&:include?.('tag: alpha_'))
+      .first
+
+  if Regexp.last_match[0]
+    (Date.today - 7).to_time + BUILD_AT < Time.parse(Regexp.last_match[1]) &&
+      yield(Regexp.last_match[2])
+    true
+  else
+    false
+  end
+end
+
+cron? && with_buildable_tag? {|tag|
+  system "git checkout #{tag} > /dev/null 2>&1"
+  puts tag
+}
--- .travis_scripts/build.sh
+++ .travis_scripts/build.sh
@@ -1,27 +1,9 @@
 ﻿#!/bin/sh
 
-#CREDENTIAL_FILE="$HOME/.config/git-credential"
-#mkdir -p $HOME/.config
 
-#echo "https://${GH_TOKEN}:@github.com" > $CREDENTIAL_FILE
-#echo "github.com:
-#- oauth_token: $GH_TOKEN
-#  user: $GH_USER"
-#> $HOME/.config/hub
+if [ $TRAVIS_EVENT_TYPE = cron ]; then
+  LIST=`git log --oneline -n 200 --decorate --pretty=format:'[%cd]%d' --date=format:'%Y/%m/%d %H:%M:%S'
+fi
 
-#git config --global user.name "kiyomura"
-#git config --global user.email "kiyomura@dwango.co.jp"
-#git config --global hub.protocol "https"
-#git config --global credential.helper "store --file=$CREDENTIAL_FILE"
-
-echo "TRAVIS_EVENT_TYPE=$TRAVIS_EVENT_TYPE"
-echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
-
-echo '------'
-pwd
-which ruby
-echo '------'
-
-git log --oneline -n 200 --decorate --pretty=format:'[%cd]%d' --date=format:'%Y/%m/%d %H:%M:%S'
 
 exit 0
