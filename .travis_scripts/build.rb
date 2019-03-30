cc4f6941ffb936231f0a7615956a3df52a6671f7
kiyomura <kiyomura@dwango.co.jp>
2019-03-30 23:16:10 +0900

    hobo

--- .travis_scripts/build.rb
+++ .travis_scripts/build.rb
@@ -1,17 +1,16 @@
 ﻿require 'date'
 require 'time'
 
-BUILD_AT = 18 * 60 * 60
 # 200履歴とればさすがに1件は alpha タグついたものあるでしょう
 MAX_OF_SEARCHING_HISTORY = 200
 
+class Integer # 読みやすくするためだけ
+  def hours; self * 60 * 60 end
+  def days; self end
+end
+
 class Symbol
-  def call(*args)
-    case
-      when args.size > 0 then -> arg {self.to_proc[arg, *args]}
-      else self.to_proc
-    end
-  end
+  def call(*args) -> arg {self.to_proc[arg, *args]} end
 end
 
 def cron?
@@ -19,7 +18,7 @@ def cron?
 end
 
 def with_buildable_tag?
-  /^(.{19}).+\(.*(alpha_[^,]+),.*\)$/ =~ <<-`EOS`
+  /^(.{19}) \(.*(alpha_[^,]+).*\)$/ =~ <<-`EOS`
       git log --oneline -n #{MAX_OF_SEARCHING_HISTORY} --decorate \
           --pretty=format:'%cd%d' --date=format:'%Y/%m/%d %H:%M:%S'
     EOS
@@ -27,8 +26,8 @@ def with_buildable_tag?
       .select(&:include?.('tag: alpha_'))
       .first
 
-  if Regexp.last_match[0]
-    (Date.today - 7).to_time + BUILD_AT < Time.parse(Regexp.last_match[1]) &&
+  if Regexp.last_match
+    (Date.today - 7.days).to_time + 18.hours < Time.parse(Regexp.last_match[1]) &&
       yield(Regexp.last_match[2])
     true
   else
@@ -36,7 +35,4 @@ def with_buildable_tag?
   end
 end
 
-cron? && with_buildable_tag? {|tag|
-  system "git checkout #{tag} > /dev/null 2>&1"
-  puts tag
-}
+cron? && with_buildable_tag? {|tag| print tag}
